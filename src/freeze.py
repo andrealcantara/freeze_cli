7edb09937019a0fc58757e644554ba9d4abfe98c
Andre G. O. Alcantara <andre.alcantara@reitoria.ifpe.edu.br>
2022-02-15 17:21:13 -0300

    Tentando capturar o error Except

--- src/freeze.py
+++ src/freeze.py
@@ -4,6 +4,8 @@ import os.path
 import subprocess
 import sys
 import logging
+import traceback
+import uuid
 
 
 class PipFreezeCLI:
@@ -16,10 +18,12 @@ class PipFreezeCLI:
             os.path.abspath(os.path.join(os.path.dirname(sys.executable), filename))
         self.config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
         if self.config.read(path_config_lib):
+            logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                filename='freeze_info.log')
             return True
         else:
             logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                                filename='compilado.log')
+                                filename='freeze_error.log')
             logging.error("Não foi possível carregar o arquivo de configuração em {}".format(path_config_lib))
             return False
 
@@ -29,7 +33,7 @@ class PipFreezeCLI:
 
     def __run(self):
         self.parser = argparse.ArgumentParser(
-            prog='Pip Freeze Custom',
+            prog='Freeze-Cli',
             description='Gerencia ambientes de importacao de pip',
             epilog='Desenvolvido por: André Alcantara',
             usage='%(prog)s [options]'
@@ -44,13 +48,17 @@ class PipFreezeCLI:
                                  format(self.config['files']['core'], self.config['main']['file']))
         self.parser.add_argument('-a', '--all', action='store_true', help='Cria o arquivo no {}.'.
                                  format(self.config['files']['all']))
-        self.parser.add_argument('--force', action='store_true')
+        self.parser.add_argument('--force', action='store_true', help='Adicionado juntamente com --dev ou --core, '
+                                                                      'força a criação do arquivo como se nao '
+                                                                      'houvesse o outro, adicionando todo o '
+                                                                      'pip-freeze nele.')
 
-        parser_args = self.parser.parse_args()
-
-        self.__createfolder()
-        if parser_args:
-            try:
+        parser_args = None
+        try:
+            # parser_args = self.parser.parse_known_args()
+            parser_args = self.parser.parse_args()
+            self.__createfolder()
+            if len(sys.argv) > 1:
                 is_forced = parser_args.force
                 if parser_args.dev:
                     self.__generate_dev(is_forced)
@@ -59,11 +67,12 @@ class PipFreezeCLI:
                 elif parser_args.all:
                     self.__generate_all()
                 self.__generate_local_requeriments()
-            except Exception as e:
-                print("Argumento inválido {}".format(e))
+            else:
+                self.parser.print_help()
                 sys.exit(1)
-        else:
-            self.parser.print_help()
+        except argparse.ArgumentError as e:
+            print("Argumento inválido {}".format(sys.argv[1:][0]))
+            print(e)
             sys.exit(1)
 
     def __createfolder(self):
@@ -71,29 +80,31 @@ class PipFreezeCLI:
             subprocess.run('mkdir -p ' + self.config['main']['config'], shell=True, capture_output=True)
 
     def __generate_dev(self, is_forced):
-        print('Inciando freeze dev')
+        logging.info('Inciando freeze dev')
         self.__freeze_another(target_req=self.config['files']['dev'], compare_req=self.config['files']['core'],
                               has_force=is_forced)
-        print('Finalizado freeze')
-        print('Criado arquivo {}'.format(self.config['files']['dev']))
+        logging.info('Finalizado freeze')
+        logging.info('Criado arquivo {}'.format(self.config['files']['dev']))
 
     def __generate_core(self, is_forced):
-        print('Inciando freeze core')
+        logging.info('Inciando freeze core')
         self.__freeze_another(target_req=self.config['files']['core'], compare_req=self.config['files']['dev'],
                               has_force=is_forced)
-        print('Finalizado freeze')
-        print('Criado arquivo {}'.format(self.config['files']['core']))
+        logging.info('Finalizado freeze')
+        logging.info('Criado arquivo {}'.format(self.config['files']['core']))
 
     def __generate_all(self):
-        print('Inciando freeze all')
+        logging.info('Inciando freeze all')
         self.__freeze_another(target_req=self.config['files']['all'])
-        print('Finalizado freeze')
-        print('Criado arquivo {}'.format(self.config['files']['all']))
+        logging.info('Finalizado freeze')
+        logging.info('Criado arquivo {}'.format(self.config['files']['all']))
 
     def __freeze_another(self, target_req='', compare_req='-h', has_force=False):
         pip_all = subprocess.check_output(self.config['command']['pip'], shell=True).decode().split('\n')
         pip_all = sorted(set(pip_all))
         other_freeze = None
+        if has_force:
+            logging.info("*** Forced ***")
         if os.path.isfile(compare_req) != has_force:
             other_freeze = subprocess.run(self.config['command']['cat']
                                           .format(compare_req), shell=True, capture_output=True)
@@ -124,10 +135,11 @@ class PipFreezeCLI:
             result.append('-r {}'.format(self.config['files']['core']))
         if exist_dev:
             result.append('-r {}'.format(self.config['files']['dev']))
-        print("Iniciando o {}".format(self.config['main']['file']))
+        logging.info("Iniciando o {}".format(self.config['main']['file']))
+        logging.info("Core_File_Exist: {}, Dev_File_Exist: {}".format(exist_core, exist_dev))
         with open(self.config['main']['file'], 'a+') as file:
             file.seek(0)
             file.truncate()
             for line in result:
                 file.write("{}\n".format(line))
-        print('Criado/Atualizado o arquivo {}'.format(self.config['main']['file']))
+        logging.info('Criado/Atualizado o arquivo {}'.format(self.config['main']['file']))
